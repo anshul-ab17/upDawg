5cb3412fbe421fcf9612c99e2a8f4d95a8e02880
Anshul Bharat <anshul.ab17x@gmail.com>
2026-02-04 23:24:34 +0530

    destructring

--- db/src/lib.rs
+++ db/src/lib.rs
@@ -1,9 +1,8 @@
-use diesel::prelude::*;
-
-use crate::config::Config;
 
 pub mod config;
 pub mod schema;
+pub mod store;
+pub mod models;
 
 
 
---
+++ db/src/store.rs
@@ -0,0 +1,15 @@
+use diesel::prelude::*;
+use crate::config::Config;
+pub struct Store {
+    pub conn: PgConnection
+}
+
+impl Store {
+    pub fn default() -> Result<Self, ConnectionError> {
+        let config = Config::default();
+        let conn = PgConnection::establish(&config.db_url)?;
+        Ok(Self {
+            conn
+        })
+    }
+}
