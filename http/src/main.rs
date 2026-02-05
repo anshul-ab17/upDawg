53e06c60aa086b6528f44326d46ec998e4d62d1d
Anshul Bharat <anshul.ab17x@gmail.com>
2026-02-05 21:01:42 +0530

    fixing all the endpointss

--- http/src/main.rs
+++ http/src/main.rs
@@ -1,5 +1,5 @@
 use poem::{
- Route, Server, post, get, handler, listener::TcpListener, web::Json
+ Route, Server, get, handler, listener::TcpListener, post, web::Json
 };
 pub mod req_input;
 pub mod req_output;
@@ -7,31 +7,26 @@ use crate::{
     req_input::CreateWebsiteInput,
     req_output::CreateWebsiteOutput
 };
-use db::store::{Store};
+
+use db::{store::Store};
 
 #[handler]
 async fn create_website(
-    Json(_data): Json<CreateWebsiteInput>,
+    Json(data): Json<CreateWebsiteInput>,
 ) -> Json<CreateWebsiteOutput> {
-    let db= Store::default();
-    let id =db.create_website().await;
-    let response = CreateWebsiteOutput {
-        id
-    };
-    Json(response)
-}
-
-#[handler]
-async fn get_website(website_id:String)-> String{
-    let db= Store::default();
-    let id = db.create_website();
-    format!("website:{}",website_id)
+    let mut db= Store::default().unwrap();
+    let website = db.create_website(String::from
+        ("803b50f8-330e-4c5c-b264-eca313136efb"), data.url).unwrap();
+        let response = CreateWebsiteOutput {
+            id:website.id
+        };
+       Json(response)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let app = Route::new()
-        .at("/website/:website_id", get(get_website))
+        .at("/website/:website_id", get(create_website))
         .at("/website", post(create_website));
         // .with(Tracing);
     Server::new(TcpListener::bind("0.0.0.0:3003"))
